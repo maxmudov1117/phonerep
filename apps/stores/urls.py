273531a9c9ece63583c641a8ca25064a773651f0
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2025-12-28 01:08:05 +0500

    feat: delete item in store

--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import StoreListView
+from .views import StoreListView, DeleteStoreItemView
 
 urlpatterns = [
-    path('', StoreListView.as_view())
+    path('', StoreListView.as_view()),
+    path('delete-item/<int:pk>/', DeleteStoreItemView.as_view(),
+         name='delete-store-item'),
 ]
